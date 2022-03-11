559cb040bff6d006bc78401f2855aa0a35f692ee
carlos noguera <carlos.noguera@riseinteractive.com>
2022-03-11 10:20:02 -0400

    Add project files.

---
+++ Condor-test/Controllers/TestsController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Condor_test.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Condor_test.Controllers
+{
+    public class TestsController : Controller
+    {
+        private const string index = "~/Views/Tests/Index.cshtml";
+        List<MoviesModel> movies = new List<MoviesModel>();
+
+        // GET: /<controller>/
+
+        public IActionResult Index()
+        {
+           
+            movies.Add(new MoviesModel(2, "Ozark", "Usa", "Netflix"));
+            movies.Add(new MoviesModel(3, "Hellbound", "Korea", "Netflix"));
+            movies.Add(new MoviesModel(4, "The Mandalorian", "Usa", "Disney+"));
+            movies.Add(new MoviesModel(5, "Law and Order", "Usa", "Star+"));
+            movies.Add(new MoviesModel(6, "The Boys", "Usa", "Amazon Prime"));
+            movies.Add(new MoviesModel(7, "The Expanse", "Usa", "Amazon Prime"));
+
+
+            return View(index, movies);
+        }
+
+        public IActionResult Edit(int movieId)
+        {
+            var Movie = movies.ElementAt(movieId);
+            return View(Movie);
+        }
+        public ActionResult Edit(MoviesModel movie)
+        {
+            var oldmovie = movies.Where(m => m.Id == movie.Id).First();
+            movies.Remove(oldmovie);
+
+            movies.Add(movie);
+            return RedirectToAction("Index");
+        }
+
+    }
+}
---
+++ Condor-test/Models/MoviesModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace Condor_test.Models
+{
+    public class MoviesModel
+    {
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Director { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public string Country { get; set; }
+        public string Platform { get; set; }
+
+        public MoviesModel()
+        {
+            Id = 1;
+            Name = "Avengers";
+         
+            Country = "USA";
+            Platform = "Cinema";
+        }
+
+        public MoviesModel(int id, string name, string country, string platform)
+        {
+            Id = id;
+            Name = name;
+            Country = country;
+            Platform = platform;
+        }
+    }
+}
